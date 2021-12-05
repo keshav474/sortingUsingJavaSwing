9bee22407c75ae27c223b575fc46696c44253c5f
keshav474 <84160543+keshav474@users.noreply.github.com>
2021-12-05 13:29:56 +0530

    buttons added

--- SortVisualizer.java
+++ SortVisualizer.java
@@ -1,38 +1,54 @@
 package com.company;
 
-
-
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Rectangle2D;
 import java.util.Random;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import javax.swing.SwingWorker;
+import javax.swing.*;
 
-public class SortVisualizer extends JPanel {
-    //    private static final long serialVersionUID = 1L;
-    private final int WIDTH = 1000, HEIGHT = WIDTH * 9 / 16;
-    private final int SIZE = 150;
+class sortingArea extends JPanel {
+//    private static final long serialVersionUID = 1L;
+    private final int WIDTH = 1000, HEIGHT = WIDTH * 8 / 16;
+    private final int SIZE = 10;
     private final float BAR_WIDTH = (float)WIDTH / SIZE;
     private float[] bar_height = new float[SIZE];
-    private SwingWorker<Void, Void> shuffler, sorter;
+    private SwingWorker<Void,Void> shuffler, sorter;
     private int current_index, traversing_index;
 
-    private SortVisualizer() throws InterruptedException {
-//        setBackground(new Color(150,50,225));
+    sortingArea() throws InterruptedException {
         setBackground(Color.WHITE);
+        setBounds(0,HEIGHT/8,WIDTH,HEIGHT);
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         initBarHeight();
+        initShuffler();
+//        insrtSorter();
 
-        insrtSorter();
+    }
+    public void sor (int x) throws InterruptedException {
+//        setBackground(Color.WHITE);
+//        setBounds(0,HEIGHT/8,WIDTH,HEIGHT);
+//        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+//        initBarHeight();
+//        initShuffler();
+
+         if(x==1)
+            insrtSorter();
+        else if(x==2)
+            selectSorter();
+        else if(x==3)
+            bubbleSorter();
+        else if(x==4)
+            iterativeMergeSorter();
 
         initShuffler();
 
+
+
+
+//      initBarHeight();
+//        insrtSorter();
     }
 
     @Override
@@ -40,8 +56,8 @@ public class SortVisualizer extends JPanel {
         super.paintComponent(g);
 
         Graphics2D g2d = (Graphics2D)g;
-        g2d.setColor(new Color(150,120,225));
         Rectangle2D.Float bar;
+        g2d.setColor(new Color(150,120,225));
         for(int i = 0; i < SIZE; i++) {
             bar = new Rectangle2D.Float(i * BAR_WIDTH, HEIGHT-bar_height[i], BAR_WIDTH, bar_height[i]);
             g2d.fill(bar);
@@ -62,7 +78,7 @@ public class SortVisualizer extends JPanel {
         g2d.fill(bar);
     }
 
-    private void insrtSorter() {
+    private void insrtSorter() throws InterruptedException {
         sorter = new SwingWorker<>() {
             @Override
             public Void doInBackground() throws InterruptedException {
@@ -74,7 +90,7 @@ public class SortVisualizer extends JPanel {
                         swap(traversing_index, traversing_index - 1);
                         traversing_index--;
 
-                        Thread.sleep(5);
+                        Thread.sleep(3);
                         repaint();
                     }
                 }
@@ -86,7 +102,7 @@ public class SortVisualizer extends JPanel {
         };
     }
 
-    private void initShuffler() {
+    private void initShuffler() throws InterruptedException {
         shuffler = new SwingWorker<>() {
             @Override
             public Void doInBackground() throws InterruptedException {
@@ -114,7 +130,7 @@ public class SortVisualizer extends JPanel {
         shuffler.execute();
     }
 
-    private void initBarHeight() {
+    public void initBarHeight() {
         float interval = (float)HEIGHT / SIZE;
         for(int i = 0; i < SIZE; i++)
         {
@@ -123,6 +139,18 @@ public class SortVisualizer extends JPanel {
             bar_height[i] = 25+random_height;
 
         }
+    }
+    public void newBarHeight() throws InterruptedException {
+        float interval = (float)HEIGHT / SIZE;
+        for(int i = 0; i < SIZE; i++)
+        {
+            int random_height = new Random().nextInt(HEIGHT-100);
+
+            bar_height[i] = 25+random_height;
+            repaint();
+
+        }
+        initShuffler();
     }
 
     private void swap(int indexA, int indexB) {
@@ -157,21 +185,103 @@ public class SortVisualizer extends JPanel {
 
     }
 
-    public static void main(String args[]) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Insertion Sort Visualizer");
+    private void bubbleSorter()
+    {
+        System.out.println("sorting");
+    }
+    private void iterativeMergeSorter()
+    {
+        System.out.println("sorting2");
+    }
+
+
+}
+
+
+class navPanel extends JPanel implements ActionListener
+{
+    private final int WIDTH = 1000, HEIGHT = WIDTH / 16;
+    JButton sortButton1=new JButton();
+    JButton sortButton2=new JButton();
+    JButton sortButton3=new JButton();
+    JButton sortButton4=new JButton();
+    private sortingArea sortaa;
+
+
+    navPanel(sortingArea sora) throws InterruptedException {
+        setBackground(Color.GRAY);
+        setPreferredSize(new Dimension(WIDTH,HEIGHT));
+        setBounds(10,0,WIDTH,HEIGHT);
+        setLayout(new GridLayout(1,4,15,15));
+        this.sortaa=sora;
+        sortButton1.setText("Insertion Sort");
+        sortButton1.addActionListener(this);
+        sortButton2.setText("Selection Sort");
+        sortButton2.addActionListener(this);
+        sortButton3.setText("Bubble Sort");
+        sortButton3.addActionListener(this);
+        sortButton4.setText("koi aur Sort");
+        sortButton4.addActionListener(this);
+
+        add(sortButton1);
+        add(sortButton2);
+        add(sortButton3);
+        add(sortButton4);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==sortButton1) {
+            try {
+                 sortaa.sor(1);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+        else if(e.getSource()==sortButton2) {
+            try {
+                sortaa.sor(2);
+
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+        else if(e.getSource()==sortButton3) {
+            try {
+                sortaa.sor(3);
+//                repaint();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+        else if(e.getSource()==sortButton4) {
+            try {
+                sortaa.newBarHeight();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+//            repaint();
+
+        }
+
+    }
+}
+public class SortVisualizer {
+    public static void main(String args[]) throws InterruptedException {
+
+            JFrame frame = new JFrame("Sort Visualizer");
+            frame.setLayout(null);
+//            frame.setSize(1000,1000*9/16);
             frame.setResizable(false);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            try {
-                frame.setContentPane(new SortVisualizer());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                System.out.println("srry");
-            }
+            sortingArea sor=new sortingArea();
+
+        frame.setContentPane(sor);
+        frame.add(new navPanel(sor));
             frame.validate();
             frame.pack();
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
-        });
+
     }
 }
